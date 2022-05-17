c856079e859c84ac89ca81d878d1d257ea129fdd
ckyqjy <ckyqjy@gmail.com>
2022-05-17 23:49:26 +0900

    feat:Set background and menu list

--- src/layout/DefaultLayout/DefaultLayout.tsx
+++ src/layout/DefaultLayout/DefaultLayout.tsx
@@ -1,12 +1,16 @@
 import type { MenuProps } from 'antd';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { readBuilderProgram } from 'typescript';
+import bg from '../../assets/img/menu-bg.jpg';
+import { url } from 'inspector';
 
 const { Header, Content, Sider } = Layout;
-
-const items1: MenuProps['items'] = ['1', '2', '3'].map(key => ({
+const serviceList = ['웹 호스팅', '웹 스토리지', 'DNS'];
+const actionList = ['신청', '조회'];
+const items1: MenuProps['items'] = serviceList.map(key => ({
   key,
-  label: `nav ${key}`,
+  label: `${key}`,
 }));
 
 const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
@@ -15,13 +19,13 @@ const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOu
 
     return {
       key: `sub${key}`,
-      label: `subnav ${key}`,
+      label: `${serviceList[index]} 신청`,
 
-      children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1;
+      children: new Array(2).fill(null).map((_, j) => {
+        const subKey = index * 2 + j + 1;
         return {
           key: subKey,
-          label: `option${subKey}`,
+          label: `${serviceList[index]} ${actionList[j]}`,
         };
       }),
     };
@@ -30,9 +34,8 @@ const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOu
 
 export default () => (
   <Layout>
-    <Header className="header">
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+    <Header style={{ backgroundImage: "url(" + "https://pixabay.com/get/g6c9c6b79e6a8b71e1f0e9baeaa1e2aae8c9639ebec6642e80034647cc55d7556a28d22b831c75e44e0d8daf5feea56652594404884234378e98b9e432ab7174a_1920.png" + ")", height: '300px', backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  className="header">
+      <Menu style={{backgroundColor: "rgba(255, 255, 255, 0.0)", marginTop: '200px', color: 'white', fontWeight: 'bolder', fontSize: '20px'}}  mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
     </Header>
     <Layout>
       <Sider width={200} className="site-layout-background">
