96c6f8ed5bc75d9ddcb51dce58b89d6a8e39706d
ckyqjy <ckyqjy@gmail.com>
2022-05-24 14:51:44 +0900

    feat:Make html files

--- src/contents/WebHostingRegister.tsx
+++ src/contents/WebHostingRegister.tsx
@@ -1,33 +1,121 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import styles from "./NotFoundPage.module.css";
-import { Form, Input, InputNumber, Button, Select } from "antd";
+import { Form, Input, InputNumber, Button, Select, Col, Card, Row } from "antd";
+import axios from "axios";
 
 export default function WebHostingRegister(): JSX.Element {
+  const [form] = Form.useForm<FormState>();
   const layout = {
-    labelCol: { span: 2 },
+    labelCol: { span: 0 },
     wrapperCol: { span: 8 },
   };
 
-  /* eslint-disable no-template-curly-in-string */
-  const validateMessages = {
-    required: "${label} is required!",
-    types: {
-      email: "${label} is not a valid email!",
-      number: "${label} is not a valid number!",
-    },
-    number: {
-      range: "${label} must be between ${min} and ${max}",
-    },
-  };
-  /* eslint-enable no-template-curly-in-string */
+  interface FormState {
+    username: string;
+    domainname: string;
+    class1: string;
+    class2: string;
+    ipaddr: string;
+    description: string;
+  }
 
-  const onFinish = (values: any) => {
-    console.log(values);
+  const onClick = () => {
+    const { username, domainname, ipaddr } = form.getFieldsValue();
+    const url = `${process.env.REACT_APP_URL}/dnsreg/${domainname}`;
+    axios.get(url).then((res) => res.data);
   };
+
+  const gra1 = "#f2f6f7";
+  const gra2 = "white";
 
   return (
     <>
+      <div
+        style={{
+          background: `linear-gradient(${gra1}, ${gra2})`,
+          width: "100%",
+          paddingBottom: "50px",
+        }}
+      >
+        <div
+          style={{
+            color: "#333",
+            fontWeight: "bolder",
+            fontSize: "30px",
+            textAlign: "center",
+            paddingTop: "30px",
+          }}
+        >
+          리눅스 웹호스팅
+        </div>
+        <div
+          style={{
+            color: "#333",
+            fontWeight: "bolder",
+            fontSize: "20px",
+            textAlign: "center",
+          }}
+        >
+          빠른 속도와 풍부한 용량을 제공하는 KB 64bit 리눅스 호스팅을
+          이용해보세요.
+        </div>
+        <div
+          style={{
+            paddingLeft: "35%",
+            paddingTop: "20px",
+            display: "flex",
+          }}
+        >
+          <div style={{}}>
+            <button
+              style={{
+                fontWeight: "bolder",
+                fontSize: "30px",
+                height: "100px",
+                width: "100px",
+                backgroundColor: "white",
+                border: "1px",
+                borderRight: "0px",
+                borderStyle: "solid",
+                borderColor: "#dddddd",
+                color: "white",
+              }}
+            >
+              www.
+            </button>
+            <input
+              style={{
+                fontSize: "30px",
+                height: "100px",
+                width: "500px",
+                display: "inline",
+                border: "1px",
+                borderLeft: "0px",
+                borderStyle: "solid",
+                borderColor: "#dddddd",
+              }}
+              placeholder="등록하실 호스팅을 입력해주세요"
+            />
+            <button
+              style={{
+                fontSize: "30px",
+                fontWeight: "bolder",
+                height: "100px",
+                width: "100px",
+                display: "inline-block",
+                backgroundColor: "#434c53",
+                border: "1px",
+                borderStyle: "solid",
+                borderColor: "#dddddd",
+                color: "white",
+              }}
+            >
+              등록
+            </button>
+          </div>
+        </div>
+      </div>
       <div
         style={{
           backgroundColor: "#0070c0",
@@ -36,48 +124,301 @@ export default function WebHostingRegister(): JSX.Element {
           textAlign: "center",
         }}
       >
-        웹 호스팅 등록 할인 이벤트 / 쿠폰코드 KBEVENT
+        호스팅 등록 할인 이벤트 / 쿠폰코드 KBEVENT
       </div>
-      <h2>웹 호스팅 신청서</h2>
-      <Form
-        {...layout}
-        name="nest-messages"
-        onFinish={onFinish}
-        validateMessages={validateMessages}
+      <div style={{ padding: "50px" }}>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={2}></Col>
+          <Col span={2}></Col>
+          <Col span={1}></Col>
+          <Col span={4}>
+            <Card
+              hoverable
+              cover={
+                <div
+                  style={{
+                    backgroundColor: "#0070c0",
+                    color: "white",
+                    textAlign: "center",
+                    fontSize: "25px",
+                    borderRadius: "10px 10px 0px 0px",
+                    height: "80px",
+                    paddingTop: "20px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  웹 1G
+                </div>
+              }
+              style={{
+                width: 240,
+                backgroundColor: "white",
+                textAlign: "center",
+                borderRadius: "10px",
+              }}
+            >
+              <div style={{ height: "100px" }}>
+                <div
+                  style={{
+                    fontSize: "15px",
+                    textDecorationLine: "line-through",
+                  }}
+                >
+                  20000원 / 년
+                </div>
+                <div style={{ fontWeight: "bolder", fontSize: "25px" }}>
+                  15000원 / 년
+                </div>
+                <br />
+                <button style={{ backgroundColor: "orange", border: "none" }}>
+                  시작가 50% 할인
+                </button>
+              </div>
+              <div style={{ paddingTop: "20px" }}>
+                트래픽 <span style={{ color: "#f46d00" }}>30G</span>일
+                <span style={{ color: "#f46d00" }}>1G</span>
+              </div>
+              <div>
+                디스크 <span style={{ color: "#f46d00" }}>500M</span>
+              </div>
+              <div>
+                <span style={{ color: "#f46d00" }}>DB</span> 무제한
+                <div style={{ height: "40px" }}></div>
+              </div>
+            </Card>
+          </Col>
+          <Col span={4}>
+            <Card
+              hoverable
+              cover={
+                <div
+                  style={{
+                    backgroundColor: "#0070c0",
+                    color: "white",
+                    textAlign: "center",
+                    fontSize: "25px",
+                    borderRadius: "10px 10px 0px 0px",
+                    height: "80px",
+                    paddingTop: "20px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  웹 3G
+                </div>
+              }
+              style={{
+                width: 240,
+                backgroundColor: "white",
+                textAlign: "center",
+                borderRadius: "10px",
+              }}
+            >
+              <div style={{ height: "100px" }}>
+                <div
+                  style={{
+                    fontSize: "15px",
+                    textDecorationLine: "line-through",
+                  }}
+                >
+                  20000원 / 년
+                </div>
+                <div style={{ fontWeight: "bolder", fontSize: "25px" }}>
+                  13500원 / 년
+                </div>
+                <br />
+                <button style={{ backgroundColor: "orange", border: "none" }}>
+                  시작가 50% 할인
+                </button>
+              </div>
+              <div style={{ paddingTop: "20px" }}>
+                트래픽 <span style={{ color: "#f46d00" }}>30G</span>일
+                <span style={{ color: "#f46d00" }}>1G</span>
+              </div>
+              <div>
+                디스크 <span style={{ color: "#f46d00" }}>500M</span>
+              </div>
+              <div>
+                <span style={{ color: "#f46d00" }}>DB</span> 무제한
+                <div style={{ height: "40px" }}></div>
+              </div>
+            </Card>
+          </Col>
+          <Col span={4}>
+            <Card
+              hoverable
+              cover={
+                <div
+                  style={{
+                    backgroundColor: "#0070c0",
+                    color: "white",
+                    textAlign: "center",
+                    fontSize: "25px",
+                    borderRadius: "10px 10px 0px 0px",
+                    height: "80px",
+                    paddingTop: "20px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  웹 10G
+                </div>
+              }
+              style={{
+                width: 240,
+                backgroundColor: "white",
+                textAlign: "center",
+                borderRadius: "10px",
+              }}
+            >
+              <div style={{ height: "100px" }}>
+                <div
+                  style={{
+                    fontSize: "15px",
+                    color: "white",
+                  }}
+                >
+                  20000원 / 년
+                </div>
+                <div style={{ fontWeight: "bolder", fontSize: "25px" }}>
+                  10000원 / 년
+                </div>
+                <br />
+                <button style={{ backgroundColor: "orange", border: "none" }}>
+                  시작가 50% 할인
+                </button>
+              </div>
+              <div style={{ paddingTop: "20px" }}>
+                트래픽 <span style={{ color: "#f46d00" }}>30G</span>일
+                <span style={{ color: "#f46d00" }}>1G</span>
+              </div>
+              <div>
+                디스크 <span style={{ color: "#f46d00" }}>500M</span>
+              </div>
+              <div>
+                <span style={{ color: "#f46d00" }}>DB</span> 무제한
+                <div style={{ height: "40px" }}></div>
+              </div>
+            </Card>
+          </Col>
+          <Col span={2}></Col>
+          <Col span={2}></Col>
+          <Col span={1}></Col>
+        </Row>
+      </div>
+      <div style={{ padding: "30px", textAlign: "center" }}>
+        * 리눅스 호스팅 설치는 PHP7.4으로 세팅됩니다. 상품별 하드 용량 및 <br />
+        * 옵션차이로 인해 하위 상품으로 서비스 변경이 불가합니다.
+      </div>
+      <div
+        style={{
+          backgroundColor: "#5ca3cf",
+          height: "380px",
+          color: "white",
+          textAlign: "center",
+        }}
       >
-        <Form.Item
-          name={["user", "name"]}
-          label="신청인"
-          rules={[{ required: true }]}
+        <div
+          style={{ fontSize: "30px", paddingTop: "70px", fontWeight: "bolder" }}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item label="서버명" rules={[{ required: true }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item label="서버 선택">
-          <Select defaultValue={"Apache"}>
-            <Select.Option value="Apache">Apache</Select.Option>
-            <Select.Option value="Nginx">Nginx</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item label="개발 언어 선택">
-          <Select defaultValue={"PHP"}>
-            <Select.Option value="PHP">PHP</Select.Option>
-            <Select.Option value="Javascript">Javascript</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item label="DB 선택">
-          <Select defaultValue={"MariaDB"}>
-            <Select.Option value="MariaDB">MariaDB</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 1 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          KB의 다양한 웹호스팅 서비스를 비교하고 선택하세요!
+        </div>
+        <div style={{ fontSize: "15px", paddingTop: "40px" }}>
+          도메인 구매시 무제한 웹호스팅 무료 서비스를 이용하실 수 있습니다.
+          <br />
+          도메인 이용 기간 내에 추가 비용 없이 무료 이용이 가능합니다.
+        </div>
+        <div
+          style={{
+            paddingTop: "30px",
+            marginBottom: "60px",
+          }}
+        >
+          <button
+            style={{
+              backgroundColor: "white",
+              color: "#5ca3cf",
+              border: "none",
+              width: "200px",
+              height: "50px",
+              fontWeight: "bolder",
+            }}
+          >
+            무제한 웹호스팅 비교하기
+          </button>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "380px",
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: "bolder",
+            fontSize: "30px",
+            paddingTop: "100px",
+          }}
+        >
+          리눅스 웹호스팅이란?
+        </div>
+        <div style={{ fontSize: "15px", paddingTop: "40px" }}>
+          리눅스(Linux) 운영체제 기반의 웹호스팅 서비스입니다.
+          <br /> LG 10기가망을 사용하여 속도가 빠르고 안정적인 서비스를 이용하실
+          수 있습니다.
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: "#f9f4f0",
+          height: "380px",
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: "bolder",
+            fontSize: "30px",
+            paddingTop: "100px",
+          }}
+        >
+          리눅스 웹호스팅 특징
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: "white",
+          height: "380px",
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: "bolder",
+            fontSize: "30px",
+            paddingTop: "100px",
+          }}
+        >
+          무료 부가서비스
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: "#f9f4f0",
+          height: "380px",
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: "bolder",
+            fontSize: "30px",
+            paddingTop: "100px",
+          }}
+        >
+          리눅스 웹호스팅 사양
+        </div>
+      </div>
     </>
   );
 }
--- src/contents/WebStorageRegister.tsx
+++ src/contents/WebStorageRegister.tsx
@@ -1,32 +1,65 @@
 import React from "react";
 import { useNavigate } from "react-router";
-import { Form, Input, InputNumber, Button, Select } from "antd";
+import { Form, Input, InputNumber, Button, Select, Row, Col, Card } from "antd";
+import axios from "axios";
 
 export default function WebStorageRegister(): JSX.Element {
+  const [form] = Form.useForm<FormState>();
   const layout = {
-    labelCol: { span: 3 },
+    labelCol: { span: 0 },
     wrapperCol: { span: 8 },
   };
 
-  /* eslint-disable no-template-curly-in-string */
-  const validateMessages = {
-    required: "${label} is required!",
-    types: {
-      email: "${label} is not a valid email!",
-      number: "${label} is not a valid number!",
-    },
-    number: {
-      range: "${label} must be between ${min} and ${max}",
-    },
-  };
-  /* eslint-enable no-template-curly-in-string */
+  interface FormState {
+    username: string;
+    domainname: string;
+    class1: string;
+    class2: string;
+    ipaddr: string;
+    description: string;
+  }
 
-  const onFinish = (values: any) => {
-    console.log(values);
+  const onClick = () => {
+    const { username, domainname, ipaddr } = form.getFieldsValue();
+    const url = `${process.env.REACT_APP_URL}/dnsreg/${domainname}`;
+    axios.get(url).then((res) => res.data);
   };
+
+  const gra1 = "#fff0e1";
+  const gra2 = "#fff8f0";
 
   return (
     <>
+      <div
+        style={{
+          background: `linear-gradient(${gra1}, ${gra2})`,
+          width: "100%",
+          paddingBottom: "50px",
+        }}
+      >
+        <div
+          style={{
+            color: "#333",
+            fontWeight: "bolder",
+            fontSize: "30px",
+            textAlign: "center",
+            paddingTop: "30px",
+          }}
+        >
+          리눅스 웹 스토리지
+        </div>
+        <div
+          style={{
+            color: "#333",
+            fontWeight: "bolder",
+            fontSize: "20px",
+            textAlign: "center",
+          }}
+        >
+          빠른 속도와 풍부한 용량을 제공하는 KB 64bit 리눅스 스토리지를
+          이용해보세요.
+        </div>
+      </div>
       <div
         style={{
           backgroundColor: "#0070c0",
@@ -35,41 +68,292 @@ export default function WebStorageRegister(): JSX.Element {
           textAlign: "center",
         }}
       >
-        웹 호스팅 등록 할인 이벤트 / 쿠폰코드 KBEVENT
+        스토리지 등록 할인 이벤트 / 쿠폰코드 KBEVENT
       </div>
-      <h2>웹 스토리지 신청</h2>
-      <Form
-        {...layout}
-        name="nest-messages"
-        onFinish={onFinish}
-        validateMessages={validateMessages}
+      <div style={{ padding: "50px" }}>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={2}></Col>
+          <Col span={2}></Col>
+          <Col span={1}></Col>
+          <Col span={4}>
+            <Card
+              hoverable
+              cover={
+                <div
+                  style={{
+                    backgroundColor: "#0070c0",
+                    color: "white",
+                    textAlign: "center",
+                    fontSize: "25px",
+                    borderRadius: "10px 10px 0px 0px",
+                    height: "80px",
+                    paddingTop: "20px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  웹 1G
+                </div>
+              }
+              style={{
+                width: 240,
+                backgroundColor: "white",
+                textAlign: "center",
+                borderRadius: "10px",
+              }}
+            >
+              <div style={{ height: "100px" }}>
+                <div
+                  style={{
+                    fontSize: "15px",
+                    textDecorationLine: "line-through",
+                  }}
+                >
+                  20000원 / 년
+                </div>
+                <div style={{ fontWeight: "bolder", fontSize: "25px" }}>
+                  15000원 / 년
+                </div>
+                <br />
+                <button style={{ backgroundColor: "orange", border: "none" }}>
+                  시작가 50% 할인
+                </button>
+              </div>
+              <div style={{ paddingTop: "20px" }}>
+                트래픽 <span style={{ color: "#f46d00" }}>30G</span>일
+                <span style={{ color: "#f46d00" }}>1G</span>
+              </div>
+              <div>
+                디스크 <span style={{ color: "#f46d00" }}>500M</span>
+              </div>
+              <div>
+                <span style={{ color: "#f46d00" }}>DB</span> 무제한
+                <div style={{ height: "40px" }}></div>
+              </div>
+            </Card>
+          </Col>
+          <Col span={4}>
+            <Card
+              hoverable
+              cover={
+                <div
+                  style={{
+                    backgroundColor: "#0070c0",
+                    color: "white",
+                    textAlign: "center",
+                    fontSize: "25px",
+                    borderRadius: "10px 10px 0px 0px",
+                    height: "80px",
+                    paddingTop: "20px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  웹 3G
+                </div>
+              }
+              style={{
+                width: 240,
+                backgroundColor: "white",
+                textAlign: "center",
+                borderRadius: "10px",
+              }}
+            >
+              <div style={{ height: "100px" }}>
+                <div
+                  style={{
+                    fontSize: "15px",
+                    textDecorationLine: "line-through",
+                  }}
+                >
+                  20000원 / 년
+                </div>
+                <div style={{ fontWeight: "bolder", fontSize: "25px" }}>
+                  13500원 / 년
+                </div>
+                <br />
+                <button style={{ backgroundColor: "orange", border: "none" }}>
+                  시작가 50% 할인
+                </button>
+              </div>
+              <div style={{ paddingTop: "20px" }}>
+                트래픽 <span style={{ color: "#f46d00" }}>30G</span>일
+                <span style={{ color: "#f46d00" }}>1G</span>
+              </div>
+              <div>
+                디스크 <span style={{ color: "#f46d00" }}>500M</span>
+              </div>
+              <div>
+                <span style={{ color: "#f46d00" }}>DB</span> 무제한
+                <div style={{ height: "40px" }}></div>
+              </div>
+            </Card>
+          </Col>
+          <Col span={4}>
+            <Card
+              hoverable
+              cover={
+                <div
+                  style={{
+                    backgroundColor: "#0070c0",
+                    color: "white",
+                    textAlign: "center",
+                    fontSize: "25px",
+                    borderRadius: "10px 10px 0px 0px",
+                    height: "80px",
+                    paddingTop: "20px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  웹 10G
+                </div>
+              }
+              style={{
+                width: 240,
+                backgroundColor: "white",
+                textAlign: "center",
+                borderRadius: "10px",
+              }}
+            >
+              <div style={{ height: "100px" }}>
+                <div
+                  style={{
+                    fontSize: "15px",
+                    color: "white",
+                  }}
+                >
+                  20000원 / 년
+                </div>
+                <div style={{ fontWeight: "bolder", fontSize: "25px" }}>
+                  10000원 / 년
+                </div>
+                <br />
+                <button style={{ backgroundColor: "orange", border: "none" }}>
+                  시작가 50% 할인
+                </button>
+              </div>
+              <div style={{ paddingTop: "20px" }}>
+                트래픽 <span style={{ color: "#f46d00" }}>30G</span>일
+                <span style={{ color: "#f46d00" }}>1G</span>
+              </div>
+              <div>
+                디스크 <span style={{ color: "#f46d00" }}>500M</span>
+              </div>
+              <div>
+                <span style={{ color: "#f46d00" }}>DB</span> 무제한
+                <div style={{ height: "40px" }}></div>
+              </div>
+            </Card>
+          </Col>
+          <Col span={2}></Col>
+          <Col span={2}></Col>
+          <Col span={1}></Col>
+        </Row>
+      </div>
+      <div
+        style={{
+          backgroundColor: "#5ca3cf",
+          height: "380px",
+          color: "white",
+          textAlign: "center",
+        }}
       >
-        <Form.Item
-          name={["user", "name"]}
-          label="신청인"
-          rules={[{ required: true }]}
+        <div
+          style={{ fontSize: "30px", paddingTop: "70px", fontWeight: "bolder" }}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item label="신청 요금제">
-          <Select defaultValue={"10GB"}>
-            <Select.Option value="10GB">10GB</Select.Option>
-            <Select.Option value="30GB">30GB</Select.Option>
-            <Select.Option value="50GB">50GB</Select.Option>
-            <Select.Option value="100GB">100GB</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item label="VIP 서비스 이용 여부">
-          <Select defaultValue={"이용"}>
-            <Select.Option value="이용">이용</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 1 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          KB의 다양한 스토리지 서비스를 비교하고 선택하세요!
+        </div>
+        <div
+          style={{
+            paddingTop: "30px",
+            marginBottom: "60px",
+          }}
+        >
+          <button
+            style={{
+              backgroundColor: "white",
+              color: "#5ca3cf",
+              border: "none",
+              width: "200px",
+              height: "50px",
+              fontWeight: "bolder",
+            }}
+          >
+            스토리지 비교하기
+          </button>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "380px",
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: "bolder",
+            fontSize: "30px",
+            paddingTop: "100px",
+          }}
+        >
+          리눅스 스토리지 서비스란?
+        </div>
+        <div style={{ fontSize: "15px", paddingTop: "40px" }}>
+          리눅스(Linux) 운영체제 기반의 스토리지 서비스입니다.
+          <br /> LG 10기가망을 사용하여 속도가 빠르고 안정적인 서비스를 이용하실
+          수 있습니다.
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: "#f9f4f0",
+          height: "380px",
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: "bolder",
+            fontSize: "30px",
+            paddingTop: "100px",
+          }}
+        >
+          리눅스 스토리지 특징
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: "white",
+          height: "380px",
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: "bolder",
+            fontSize: "30px",
+            paddingTop: "100px",
+          }}
+        >
+          무료 부가서비스
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: "#f9f4f0",
+          height: "380px",
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: "bolder",
+            fontSize: "30px",
+            paddingTop: "100px",
+          }}
+        >
+          리눅스 스토리지 사양
+        </div>
+      </div>
     </>
   );
 }
